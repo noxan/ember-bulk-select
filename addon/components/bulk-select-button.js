ccbb86ba6195c2c127e93ad0549402ba3f2f42ab
Richard <hello@noxan.net>
2017-01-23 16:02:47 +0100

    extract bulk select button component from webdock/client, credits to @fr3akout

--- addon/components/bulk-select-button.js
+++ addon/components/bulk-select-button.js
@@ -2,5 +2,16 @@ import Ember from 'ember';
 import layout from '../templates/components/bulk-select-button';
 
 export default Ember.Component.extend({
-  layout
+  layout,
+
+  tagName: '',
+
+  data: null,
+  select: null,
+
+  actions: {
+    selectObject(object, event) {
+      this.get('select')(object, event);
+    },
+  },
 });
