cd93b2645a3b9ed0735688e54b98e21159629bae
Richard <hello@noxan.net>
2017-01-23 16:03:08 +0100

    extract bulk toggle button component from webdock/client, credits to @fr3akout

--- addon/components/bulk-toggle-button.js
+++ addon/components/bulk-toggle-button.js
@@ -2,5 +2,16 @@ import Ember from 'ember';
 import layout from '../templates/components/bulk-toggle-button';
 
 export default Ember.Component.extend({
-  layout
+  layout,
+
+  tagName: '',
+
+  selected: null,
+  toggle: null,
+
+  actions: {
+    toggleSelection(selected) {
+      this.get('toggle')(selected);
+    },
+  },
 });
